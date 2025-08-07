a11c5ab12bff5c1054b85b997631b29d96341cd0
MathisDS15 <mathisds1506@gmail.com>
2025-08-07 17:33:47 +0100

    I created two HTML pages, one for the home page and the other for your password results. The CSS file doesn't work for the moment. However, the script with passwordCheckerPhase3 works !

--- PasswordCheckerPhase3/app.py
+++ PasswordCheckerPhase3/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
+from PasswordCheckerPhase3.backend.passwordCheckerPhase3 import passwordChecker
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='frontend/templates', static_folder='frontend/static')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/check', methods=['GET','POST'])
+def check_password():
+    if request.method == 'POST':
+        password = request.form.get('password')
+        result = passwordChecker(password)
+
+        strength_message = {
+            "Verry Strong": "Your password is very strong. Excellent choice!",
+            "Strong": "Your password is strong. Good job!",
+            "Medium": "Your password is medium. Consider making it stronger.",
+            "Weak": "Your password is weak. Please consider improving it.",
+            "Very Weak": "Your password is very weak. It needs significant improvement."
+        }
+
+        return render_template('passwordResult.html', strength_message=strength_message[result['strength']],
+                               is_pwned=result['pwned'])
 
 
 if __name__ == '__main__':
